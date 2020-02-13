c2f79313f0d6e8545092c523f6d5375d5456d557
vagrant <vagrant@homestead>
2020-02-13 08:17:49 +0000

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -20,11 +21,22 @@ class ResetPasswordController extends Controller
     */
 
     use ResetsPasswords;
-
     /**
      * Where to redirect users after resetting their password.
      *
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    protected function sendResetResponse(Request $resquest, $response)
+    {
+        session()->flash('success', '密码更新成功，您已成功登陆!');
+        return redirect($this->redirectPath());
+    }
 }
