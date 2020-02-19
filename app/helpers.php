41423dfd7aabf668e384211c288d7d21c8956bac
vagrant <vagrant@homestead>
2020-02-19 06:29:53 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,12 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function category_nav_active($category_id)
+{
+
+    // -- active_class 如果传参满足指定条件 ($condition) ，此函数将返回 $activeClass，否则返回 $inactiveClass。
+    // if_route () - 判断当前对应的路由是否是指定的路由；
+    // if_route_param () - 判断当前的 url 有无指定的路由参数。
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -1,5 +1,5 @@
 @if (count($topics))
-  <ul class="list-unstyled">
+  <ul class="media-list">
     @foreach ($topics as $topic)
       <li class="media">
         <div class="media-left">
@@ -21,7 +21,7 @@
 
           <small class="media-body meta text-secondary">
 
-            <a class="text-secondary" href="#" title="{{ $topic->category->name }}">
+            <a class="text-secondary" href="{{ route('categories.show',$topic->category_id) }}" title="{{ $topic->category->name }}">
               <i class="far fa-folder"></i>
               {{ $topic->category->name }}
             </a>
