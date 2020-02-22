afabba34c28f8cbc49bbccba378c3dd5a4afdd02
vagrant <vagrant@homestead>
2020-02-22 09:22:10 +0000

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -12,13 +13,18 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        // Xss过滤
         $topic->body = clean($topic->body, 'user_toppic_body');
         //生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+    }
 
+    public function saved(Topic $topic)
+    {
         //如果slug字段无内容，及使用翻译器对title进行翻译
         if (!$topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
