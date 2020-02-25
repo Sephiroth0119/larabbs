c7fa22e59e6a84f9471798380cd84881dd988a3a
vagrant <vagrant@homestead>
2020-02-25 07:11:51 +0000

    活跃用户

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
+use App\Models\User;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $request, Topic $topic)
+    public function show(Category $category, Request $request, Topic $topic, User $user)
     {
         //读取分类ID关联的话题，并按每20条分页
         $topics = $topic->withOrder($request->order)->where('category_id', $category->id)->with('user', 'category')->paginate(20);
-        return view('topics.index', compact('topics', 'category'));
+
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics', 'category', 'active_users'));
     }
 }
